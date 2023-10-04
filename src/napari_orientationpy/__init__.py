a2a2737f728bb4397e346643d26e8bc93d1f3c18
Mallory Wittwer <mallory.wittwer@epfl.ch>
2023-10-04 18:23:39 +0200

    v0.0.4

--- src/napari_orientationpy/__init__.py
+++ src/napari_orientationpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 from ._widget import OrientationWidget
 from ._widget_plotting import OrientationPlottingWidget
 # from ._widget_boxes import OrientationBoxesWidget
